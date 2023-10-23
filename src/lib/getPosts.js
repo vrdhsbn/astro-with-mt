196795155355b170a4ddc46823b590373b5f1ca6
Naoki Komazawa <komazawa@ekzm.co.jp>
2023-10-23 17:59:32 +0900

    commit

--- src/lib/getPosts.js
+++ src/lib/getPosts.js
@@ -1,11 +1,10 @@
 import axios from 'axios'
 
-const url = 'http://localhost/cgi-bin/MT7-R5402/mt-data-api.cgi/v5/sites/1/entries'
+const url = import.meta.env.MT_ENDPOINT_URL
 
 async function getPosts() {
   console.log('fetching...')
   const res = await axios.get(url)
-  console.log(res)
   console.log('done.')
 
   return res.data
